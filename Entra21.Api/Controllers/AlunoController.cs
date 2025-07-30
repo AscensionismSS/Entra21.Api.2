109ff5a4ae783bf9cdff09941e501f4ab93bd30a
AscensionismSS <gabriel.douglas.xd@gmail.com>
2025-07-29 21:26:57 -0300

    Adicionar arquivos de projeto.

---
+++ Entra21.Api/Controllers/AlunoController.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Modelo.Domain;
+
+
+namespace Entra21.Api.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AlunoController : Controller
+    {
+        [HttpGet("BuscarDadosAluno/{id}")]
+        public IActionResult BuscarDadosAluno(int id)
+        {
+            try
+            {
+                Aluno aluno = new Aluno();
+
+                aluno.Id = 1;
+                aluno.Nome = "João da Silva";
+                aluno.Idade = 20;
+                aluno.Cep = "12345-678";
+
+                return Ok(aluno);
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest();
+            }
+        }
+    }
+}
